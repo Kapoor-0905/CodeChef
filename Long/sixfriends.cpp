a81a94a8fe9fee84f5c8120933635a6e32d52f3e
avnigoyal25 <91952706+avnigoyal25@users.noreply.github.com>
2022-06-13 19:44:16 +0530

    Add files via upload

---
+++ Long/sixfriends.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        if(x*3==y*2)
+        {
+            cout<<x*3<<endl;
+        }
+        else if((x*3) < (y*2))
+        {
+            cout<<x*3<<endl;
+        }
+        else
+        {
+            cout<<y*2<<endl;
+        }
+    }
+}
