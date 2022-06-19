a9c6a8fb48216c6aa756896405d9616b6928ff90
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-06-19 20:29:53 +0530

    Final .py Commit

---
+++ Long/May Long Two/PRACTICEPERF/Solution.py
@@ -0,0 +1,12 @@
+p,q,r,s = input().split()
+count = 0
+if(int(p) >=10):
+    count += 1
+if(int(q) >= 10):
+    count+=1
+if(int(r)>= 10):
+    count+=1
+if(int(s)>=10):
+    count+=1
+
+print(count)
