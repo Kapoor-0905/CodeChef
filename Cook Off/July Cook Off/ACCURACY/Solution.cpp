14419409e9582c61b532b0c56407de09d626d98d
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-07-04 21:25:19 +0530

    Final .cpp commit

---
+++ Cook Off/July Cook Off/ACCURACY/Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+  int t,i,x,res,ult,fi;
+  cin>>t;
+  for(i=0;i<t;i++){
+    cin>>x;
+    if(x==0 || x%3==0){
+      cout<<"0"<<"\n";
+    }
+    else{
+      res = x/3;
+      ult = res+1;
+      fi = ult*3;
+      cout<<fi-x<<"\n";
+    }
+  }
+  return 0;
+}
