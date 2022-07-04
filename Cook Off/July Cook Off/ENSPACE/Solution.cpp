a572b1c9efbf43858671e04174ce304ed9f84905
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-07-04 21:31:52 +0530

    Final .cpp commit

---
+++ Cook Off/July Cook Off/ENSPACE/Solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+  int t,i,n,x,y;
+  cin>>t;
+  for(i=0;i<t;i++){
+    cin>>n>>x>>y;
+    if(x+(y*2) <= n) cout<<"YES"<<"\n";
+    else cout<<"NO"<<"\n";
+  }
+  return 0;
+}
