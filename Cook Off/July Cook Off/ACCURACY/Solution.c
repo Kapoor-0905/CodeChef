694dc19dc4b46fdab3e893db2b9ae35adcdbf982
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-07-04 21:19:09 +0530

    Final .c commit

---
+++ Cook Off/July Cook Off/ACCURACY/Solution.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int t,i,x,res,ult,fi;
+  scanf("%d",&t);
+  for(i=0;i<t;i++){
+    if(x==0 || x%3==0){
+      printf("0");
+    }
+    else{
+      res = x/3;
+      ult = res+1;
+      fi = ult*3;
+      printf("%d",fi-x);
+    }
+  }
+  return 0;
+}
