178cded7cec610a4d573a70ab33bf1c1bffc558d
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-07-09 14:36:53 +0530

    Final .c commir

--- Starters/Starters 42/EQUALDIST/Solution.c
+++ Starters/Starters 42/EQUALDIST/Solution.c
@@ -1,2 +1,13 @@
-dzgtng
+#include <stdio.h>
 
+int main() {
+    int a,b,i,t;
+    scanf("%d",&t);
+    for(i=0;i<t;i++){
+        scanf("%d%d",&a,&b);
+        if((a+b)%2 == 0)
+        printf("YES\n");
+        else printf("NO\n");
+    }
+	return 0;
+}
