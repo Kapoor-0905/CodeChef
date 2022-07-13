b785edbe78f854159d5b543dbf537104a831cebe
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-07-13 23:08:58 +0530

    Final .c commit

---
+++ Starters/Starters 47/CHEFONDATE/Solution.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int t,i,x,y;
+	scanf("%d",&t);
+	for(i=0;i<t;i++){
+	    scanf("%d%d",&x,&y);
+	    if(y<=x) printf("YES\n");
+	    else printf("NO\n");
+	}
+	return 0;
+}
