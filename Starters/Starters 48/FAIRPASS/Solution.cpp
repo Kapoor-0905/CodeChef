f1d079df0e53c98261644db83fc2f3f23306a9c6
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-07-29 23:52:50 +0530

    Final .cpp commit

---
+++ Starters/Starters 48/FAIRPASS/Solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,i,n,k;
+	cin>>t;
+	for(i=0;i<t;i++){
+	    cin>>n>>k;
+	    if(k>n) cout<<"YES"<<"\n";
+	    else cout<<"NO"<<"\n";
+	}
+	return 0;
+}
