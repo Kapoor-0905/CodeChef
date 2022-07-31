7980a229bd8b14b06395698fc5856adde2392c66
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-07-31 21:48:16 +0530

    Create Solution.cpp

---
+++ Long/July Long Two/INSURANCE/Solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,i,x,y;
+	cin>>t;
+	for(i=0;i<t;i++){
+	    cin>>x>>y;
+	    if(y<=x) cout<<n<<"\n";
+	    else cout<<x<<"\n";
+	}
+	return 0;
+}
