48a0d7b2ff2c8d4cadcb4b95ee72af588a67d161
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-08-05 23:17:21 +0530

    Create Doubt.java

---
+++ Starters/Starters 40/DOUBTSUPPORT/Doubt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Doubt{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int x = input.nextInt();
+    if(x<=1600){
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+  }
+}
