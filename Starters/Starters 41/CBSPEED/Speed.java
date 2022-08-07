6bef64477951123f07db9ceceb232f2581449bf6
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-08-07 22:45:45 +0530

    Create Speed.java

---
+++ Starters/Starters 41/CBSPEED/Speed.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Speed{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int x = input.nextInt();
+    int y = input.nextInt();
+    if(x<y){
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+  }
+}
