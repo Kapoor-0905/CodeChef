1397934adbc7c4d62e6c75eb286a5e402fafa165
Ashutosh Kapoor <101355193+Kapoor-0905@users.noreply.github.com>
2022-08-23 23:43:22 +0530

    Create Solution.cpp

---
+++ Lunch Time/August Lunchtime/CANDYDIST/Solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	int t,i,n,m,rem,dem;
+	cin>>t;
+	for(i=0;i<t;i++){
+	    cin>>n>>m;
+	    rem = n%m;
+	    dem = n/m;
+	    if(rem==0 && dem%2==0) cout<<"YES"<<"\n";
+	    else cout<<"NO"<<"\n";
+	}
+	return 0;
+}
